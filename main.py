18852e1dbbcff812e9591ba379fb402c31f545ee
Denys Morozov <ajax3101@gmail.com>
2023-02-21 22:40:07 +0200

    add base simple

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ import uvicorn
 from todo.app import app
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
+    uvicorn.run("main:app", host="127.0.0.1", port=os.getenv("PORT", default=5000), log_level="info")
