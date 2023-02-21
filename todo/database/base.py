7c334292b5799dbb1a236d596f45e77a20baef93
Denys Morozov <ajax3101@gmail.com>
2023-02-21 19:02:52 +0200

    create databases strukture

--- todo/database/base.py
+++ todo/database/base.py
@@ -1,6 +1,9 @@
 import os
+from sqlalchemy.orm import declarative_base
 
+BASE_DIR = os.path.dirname(os.path.abspath(__name__))
+db_path = os.path.join(BASE_DIR, 'todo', 'database', 'DB')
+if not os.path.exists(db_path):
+    os.makedirs(db_path)
 
-
-BASE_DIR=os.path.dirname(os.path.abspath(__name__))
-db_path =  os.path.join(BASE_DIR, 'todo', 'database', 'DB')
+Base = declarative_base()
--- todo/models.py
+++ todo/models.py
@@ -1,1 +1,10 @@
-class ToDo
+from todo.database.base import Base
+from sqlalchemy import Column, String, Boolean, Integer
+
+
+class ToDo(Base):
+    __tablename__='todos'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column (String)
+    is_complete = Column(Boolean, default=False)
